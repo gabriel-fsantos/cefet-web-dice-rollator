64faa110de1bba0635ed0cc1c0192b559a646a93
Gabriel Ferreira dos Santos <biel0fds@gmail.com>
2020-02-18 11:59:01 -0300

    Inicio

--- principal.js
+++ principal.js
@@ -11,4 +11,53 @@
 // .quantidade: todos os input[type=number] com a quantidade de dados a serem rolados
 // #quantidadeD{4,6,8,10,12,20}: um ID para cada input[type=number] com a quantidade
 
+function rolar(){
+    document.getElementById("recipienteResultados").classList.remove("oculto");
+    var resultado = [];
+    var dados = [];
+    dados[0] = document.getElementById("quantidadeD4").value;
+    dados[1] = document.getElementById("quantidadeD6").value;
+    dados[2]= document.getElementById("quantidadeD8").value;
+    dados[3] = document.getElementById("quantidadeD10").value;
+    dados[4] = document.getElementById("quantidadeD12").value;
+    dados[5] = document.getElementById("quantidadeD20").value;
+    var pos = 0;
+    //console.log(dados);
+    for(i=0; i<6; i++){
+        for(j=0; j<dados[i]; j++){
+            switch (i) {
+                case 0:
+                    resultado[pos] = Math.ceil(Math.random() * 4);
+                    pos++;
+                    break;
+                case 1:
+                    resultado[pos] = Math.ceil(Math.random() * 6);
+                    pos++;
+                    break;
+                case 2:
+                    resultado[pos] = Math.ceil(Math.random() * 8);
+                    pos++;
+                    break;
+                case 3:
+                    resultado[pos] = Math.ceil(Math.random() * 10);
+                    pos++;
+                    break;
+                case 4:
+                    resultado[pos] = Math.ceil(Math.random() * 12);
+                    pos++;
+                    break;
+                case 5:
+                    resultado[pos] = Math.ceil(Math.random() * 20);
+                    pos++;
+                    break;
+            }
+        }
+    }
 
+    //console.log(resultado);
+    var string = ""
+    for(i of resultado){
+        string.concat(i, "+ ");
+    }
+    console.log(string);
+}
